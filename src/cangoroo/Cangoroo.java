20569eba3cdf7ce1b46b2fd95500074e507663e2
Suporte-Agaxtur <suporte@agaxtur.com.br>
2016-02-22 11:44:40 -0300

    Commit

--- src/cangoroo/Cangoroo.java
+++ src/cangoroo/Cangoroo.java
@@ -16,7 +16,7 @@ public class Cangoroo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello, World!!!");
+        System.out.println("Hello, World!");
         int x = 10;
         System.out.println(x);
     }
