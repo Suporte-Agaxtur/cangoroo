6eec4e5659ddc234a906e3d8a3e0253f8e7a9c9b
Suporte-Agaxtur <suporte@agaxtur.com.br>
2016-02-23 09:43:01 -0300

    Commit

--- src/cangoroo/Cangoroo.java
+++ src/cangoroo/Cangoroo.java
@@ -15,21 +15,8 @@ public class Cangoroo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello, World!");
-        int x = 9;
-        int y = 15;
-        System.out.println(x);
         
-        if (x == 10) {
-            System.out.println("Hello");
-        }
-        else {
-            System.out.println("Netbeans é melhor!");
-        }
-        
-        
-        
+                        
     }
     
 }
